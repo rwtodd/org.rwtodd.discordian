3ce5c8a1b7c58dc1a02e40b00f79067cf029e146
Richard Todd <rwtodd@users.noreply.github.com>
2020-08-23 08:47:09 -0500

    adjusted error messages

--- cli/main.scala
+++ cli/main.scala
@@ -10,10 +10,15 @@ object Cmd {
 			println(args.length match {
 				case 0 => new Date().format(todayFmt)
 				case 1 => new Date().format(args(0))
-				case _ => throw new Exception("incorrect arguments!")
+				case 3 => new Date(args(0).toInt, args(1).toInt, args(2).toInt).format(otherFmt)
+				case 4 => new Date(args(1).toInt, args(2).toInt, args(3).toInt).format(args(0))
+				case _ => throw new Exception("Wrong number of arguments!")
 			})
 		} catch {
-			case e:Throwable => System.err.println(e); System.exit(1)
+			case e:Throwable =>
+				System.err.println("Error: " + e.getMessage)
+				System.err.println("Usage: ddate [fmt] [year month day]")
+				System.exit(1)
 		}
 	}
 }
