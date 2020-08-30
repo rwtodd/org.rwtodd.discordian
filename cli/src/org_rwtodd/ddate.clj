41e0af9ed0e77ecf39a7f4c8cd76535c2e29c49c
Richard Todd <rwtodd@users.noreply.github.com>
2020-08-30 08:55:52 -0500

    added *err* errors and System/exit for bad cases

--- cli/src/org_rwtodd/ddate.clj
+++ cli/src/org_rwtodd/ddate.clj
@@ -2,17 +2,18 @@
   (:require [org-rwtodd.discordian.core :as d])
   (:gen-class))
 
-(defn- to-ints [args] (map (fn [x] (Integer/parseInt x)) args))
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (try
-    (case (count args)
-      0 (println (d/format "Today is %{%A, the %e day of %B%} in the YOLD %Y%N%nCelebrate %H"
-                           (d/date)))
-      1 (println (d/format (first args) (d/date)))
-      3 (println (d/format  "%{%A, %B %d%}, %Y YOLD" (apply d/date (to-ints args))))
-      4 (println (d/format (first args) (apply d/date (to-ints (rest args)))))
-      (println "Wrong number of arguments!"))
-    (catch Exception e (println "Usage: ddate [fmt] [y m d]"))))
+  (letfn [(to-ints [args] (map #(Integer/parseInt %) args))]
+    (try
+      (case (count args)
+        0 (println (d/format "Today is %{%A, the %e day of %B%} in the YOLD %Y%N%nCelebrate %H"
+                             (d/date)))
+        1 (println (d/format (first args) (d/date)))
+        3 (println (d/format  "%{%A, %B %d%}, %Y YOLD" (apply d/date (to-ints args))))
+        4 (println (d/format (first args) (apply d/date (to-ints (rest args)))))
+        (throw (Exception. "Wrong number of arguments!")))
+      (catch Exception e
+        (.println *err* "Usage: ddate [fmt] [y m d]")
+        (System/exit 1)))))
