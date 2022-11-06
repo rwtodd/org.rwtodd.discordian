290763cdbe60ca32a45b17614cde338d45eea0e1
Richard Todd <rwtodd@users.noreply.github.com>
2022-11-06 15:06:21 -0600

    converted ddate command to groovy

--- ddate/src/main/groovy/ddate/Cmd.groovy
+++ ddate/src/main/groovy/ddate/Cmd.groovy
@@ -1,13 +1,13 @@
-package org.rwtodd.ddate;
+package ddate
 
-import org.rwtodd.discordian.DiscordianDate;
+import org.rwtodd.discordian.DiscordianDate as DD
 
 /**
  * The CLI runner for the ddate utility
  *
  * @author rwtodd
  */
-public class Cmd {
+class Cmd {
 
     private static String removeInitialPlus(String s) {
         if (s.isEmpty() || (s.charAt(0) != '+')) {
@@ -21,12 +21,12 @@ public class Cmd {
         final var otherFmt = "%{%A, %B %d%}, %Y YOLD";
         try {
             System.out.println(switch (args.length) {
-                case 0 -> new DiscordianDate().format(todayFmt);
-                case 1 -> new DiscordianDate().format(removeInitialPlus(args[0]));
-                case 3 -> new DiscordianDate(Integer.parseInt(args[0]),
+                case 0 -> new DD().format(todayFmt);
+                case 1 -> new DD().format(removeInitialPlus(args[0]));
+                case 3 -> new DD(Integer.parseInt(args[0]),
                         Integer.parseInt(args[1]),
                         Integer.parseInt(args[2])).format(otherFmt);
-                case 4 -> new DiscordianDate(Integer.parseInt(args[1]),
+                case 4 -> new DD(Integer.parseInt(args[1]),
                         Integer.parseInt(args[2]),
                         Integer.parseInt(args[3])).format(removeInitialPlus(args[0]));
                 default -> throw new Exception("Wrong number of arguments!");
