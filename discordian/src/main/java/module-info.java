eea6a817d5318b4d550ce5fa346f01caabf637b5
Richard Todd <rwtodd@users.noreply.github.com>
2022-12-11 00:53:57 -0600

    got the library pushed to maven

--- discordian/src/main/java/module-info.java
+++ discordian/src/main/java/module-info.java
@@ -1,3 +1,8 @@
+/**
+ * This is a module for calculating properties of the discordian date.
+ *
+ * @author Richard Todd
+ */
 module org.rwtodd.discordian {
   exports org.rwtodd.discordian;
 }
--- discordian/src/main/java/org/rwtodd/discordian/DiscordianDate.java
+++ discordian/src/main/java/org/rwtodd/discordian/DiscordianDate.java
@@ -2,13 +2,13 @@ package org.rwtodd.discordian;
 
 import java.text.NumberFormat;
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.temporal.ChronoUnit;
 
 /**
- * Represents a formattable discordian date.
+ * Represents a discordian date.  It can be queried for info or formatted into
+ * a string.
  *
- * @author rwtodd
+ * @author Richard Todd
  */
 public class DiscordianDate {
 
@@ -53,7 +53,7 @@ public class DiscordianDate {
     private final boolean tibs_p;
 
     /**
-     * Initialize a DiscordianDate with a gregorian LocalDate.
+     * Initialize a {@code DiscordianDate} with a gregorian {@link LocalDate}.
      *
      * @param ld the local date
      */
@@ -67,7 +67,7 @@ public class DiscordianDate {
     }
 
     /**
-     * Initialize a DiscordianDate with the year/month/day.
+     * Initialize a {@code DiscordianDate} with the year/month/day.
      *
      * @param y the year (YYYY)
      * @param m the month (MM)
@@ -78,44 +78,92 @@ public class DiscordianDate {
     }
 
     /**
-     * Initialize a DiscordianDate with the current date.
+     * Initialize a {@code DiscordianDate} with the current date.
      */
     public DiscordianDate() {
         this(LocalDate.now());
     }
 
+    /**
+     * Get the Discordian year.
+     * @return the year
+     */
     public int getYear() {
         return gdate.getYear() + 1166;
     }
 
+    /**
+     * Get the day of the discordian season. There are 73 days per season.
+     * @return the day in the season (1 - 73)
+     */
     public int getDayOfSeason() {
         return season_day;
     }
 
+    /**
+     * Find ouf if it happens to be <i>St. Tib's Day</i>.
+     * @return well is it??
+     */
     public boolean isTibs() {
         return tibs_p;
     }
 
+    /**
+     * Get the name of the current season.  The discordian calendqr has five of them.
+     *
+     * @return the current season.
+     * @see #getShortSeason()
+     */
     public String getSeason() {
         return (tibs_p) ? TIBS : SEASONS[2 * (adjusted_yday / 73)];
     }
 
+    /**
+     * Get an abbreviated name for the current season.
+     *
+     * @return the abbreviated name
+     * @see #getSeason()
+     */
     public String getShortSeason() {
         return (tibs_p) ? TIBS : SEASONS[2 * (adjusted_yday / 73) + 1];
     }
 
+    /**
+     * Get the name of the current weekday. There are 5 days in the discordian week.
+     * If it happens to be St. Tib's Day, the return string will say so, since it isn't
+     * actually a weekday.
+     *
+     * @return the weekday
+     * @see #getShortWeekday()
+     */
     public String getWeekday() {
         return (tibs_p) ? TIBS : DAYS[2 * (adjusted_yday % 5)];
     }
 
+    /**
+     * Get the abbreviated name of the current weekday. There are 5 days in the discordian week.
+     * If it happens to be St. Tib's Day, the return string will say so, since it isn't
+     * actually a weekday.
+     *
+     * @return the weekday
+     * @see #getWeekday()
+     */
     public String getShortWeekday() {
         return (tibs_p) ? TIBS : DAYS[2 * (adjusted_yday % 5) + 1];
     }
 
+    /**
+     * Find out if today is a holy day.  There are two per season.
+     * @return well is it???
+     */
     public boolean isHolyDay() {
         return (season_day == 5) || (season_day == 50);
     }
 
+    /**
+     * Get the name of the current holy day, or the empty string if it isn't one.
+     * @return the name.
+     */
     public String getHolyDayName() {
         return switch (season_day) {
             case 5 -> HOLYDAY_5[adjusted_yday / 73];
@@ -124,10 +172,36 @@ public class DiscordianDate {
         };
     }
 
+    /**
+     * Find out how many days there are until <i>X-Day</i> (which, in the Gregorian
+     * Calendar, is July 5, 8661).
+     *
+     * @return how many days until X-Day
+     */
     public long getDaysTilXDay() {
         return gdate.until(LocalDate.of(8661, 7, 5), ChronoUnit.DAYS);
     }
 
+    /**
+     * <p>Generate a string for the date according to the given format string.  The accepted format
+     * strings correspond very closely with those accepted by the Linux {@code ddate} command.</p>
+     *
+     * <ul>
+     *     <li>{@code %A/%a} = the weekday/the abbreviated weekday</li>
+     *     <li>{@code %B/%b} = the season/the abbreviated season</li>
+     *     <li>{@code %d/%e} = the day of season/the ordinal day of season</li>
+     *     <li>{@code %H} = the name of the Holy Day</li>
+     *     <li>{@code %X} = days until X-Day</li>
+     *     <li>{@code %Y} = the year</li>
+     *     <li>{@code %N} = skip the rest unless it's a holy day</li>
+     *     <li><code>%{ ... %}</code> = if it's Tibs, announce that and skip the "...".
+     *     Otherwise, format whatever is in the "..." part.</li>
+     *     <li>{@code %%, %n, %t} = a literal '%', newline, or tab</li>
+     *     <li>{@code %.} = give a random exclamation</li>
+     * </ul>
+     * @param fstr the format string
+     * @return the generated string
+     */
     public String format(String fstr) {
         final var last = fstr.length();
         final var buff = new StringBuilder(last * 2);
@@ -142,8 +216,8 @@ public class DiscordianDate {
                     case 'B' -> buff.append(getSeason());
                     case 'b' -> buff.append(getShortSeason());
                     case 'd' -> buff.append(getDayOfSeason());
-                    case 'e' -> buff.append(season_day)
-                            .append(ordinalSuffix(season_day));
+                    case 'e' -> buff.append(getDayOfSeason())
+                            .append(ordinalSuffix(getDayOfSeason()));
                     case 'H' -> buff.append(getHolyDayName());
                     case 'n' -> buff.append('\n');
                     case 't' -> buff.append('\t');
