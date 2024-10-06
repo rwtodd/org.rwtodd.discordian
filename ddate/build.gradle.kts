37f25dcdd58a23a4db51391677a3a5333a542a33
Richard Todd <8326233+rwtodd@users.noreply.github.com>
2024-10-06 14:50:03 -0500

    updated to kotlin-based gradle build

---
+++ ddate/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+   java
+   application
+}
+
+sourceSets {
+    main {
+        java { setSrcDirs(listOf("src")) }
+    }
+}
+
+dependencies {
+    implementation(project(":discordian"))
+    implementation("org.rwtodd:org.rwtodd.args:2.0.1")
+}
+
+application {
+    mainModule = "ddate"
+    mainClass = "ddate.Cmd"
+}
+
+tasks.register("runcmd") {
+    dependsOn("assemble")
+    doLast {
+        var cp = sourceSets["main"].runtimeClasspath.filter { it.name.endsWith(".jar") }.getFiles().toList().map { it.getAbsolutePath() }
+        cp += File(projectDir,"build/libs/ddate-1.0.0.jar").getAbsolutePath()
+        var joinChar = ":"
+        if ( (System.getProperties().get("os.name") as String).lowercase().contains("windows")) {
+            joinChar = ";"
+        }
+        println("java -p '${cp.joinToString(joinChar)}' -m ddate")
+    }
+}
+
---
+++ discordian/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+    `jvm-test-suite`
+
+    // upload to Maven
+    `maven-publish`
+    signing
+}
+
+base {
+  archivesName = "org.rwtodd.discordian"
+}
+
+dependencies {
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "org.rwtodd.discordian"
+            from(components["java"])
+            pom {
+                name = "org.rwtodd.discordian"
+                description = "A discordian date library"
+                url = "https://github.com/rwtodd/org.rwtodd.discordian"
+                //properties = mapOf( "" to "" )
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "rwtodd"
+                        name = "Richard Todd"
+                        email = "rwtodd@users.noreply.github.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/rwtodd/org.rwtodd.discordian.git"
+                    developerConnection = "scm:git:https://github.com/rwtodd/org.rwtodd.discordian.git"
+                    url = "https://github.com/rwtodd/org.rwtodd.discordian"
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = project.findProperty("ossrhUsername") as String? ?: ""
+                password = project.findProperty("ossrhPassword") as String? ?: ""
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
